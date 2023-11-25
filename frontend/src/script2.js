8a9612bd7c46e88daa3309bdbf506aa35134254d
Kartik Chinda <kartik.chinda@gmail.com>
2023-11-26 05:24:35 +0530

    Added routing for the HTML pages

--- frontend/src/script2.js
+++ frontend/src/script2.js
@@ -1,11 +1,16 @@
-function changePg2(){
-    document.getElementById('formid').addEventListener('submit', function(event) {
-      // Prevent the default form submission behavior
-      event.preventDefault();
-  
-      // Redirect to 'index2.html' immediately
-      window.location.href = 'http://127.0.0.1:5500/src/html/index3.html';
-    
+function changePg2() {
+  document.getElementById('formid').addEventListener('submit', function (event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    // Redirect to 'index2.html' immediately
+    // call at the end of the last function on the page. 
+    relocate();
+
   });
-  }
+}
 changePg2();
+
+function relocate() {
+  location.replace('index3.html')
+}
