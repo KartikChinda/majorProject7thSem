ac29d4c92cb3590fd6fba7b5c8273bc21274e87c
Kartik Chinda <kartik.chinda@gmail.com>
2023-11-25 19:13:04 +0530

    Init folders

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+// import cors from 'cors';
+// import userRoutes from './Routes/users.js'
+// import * as dotenv from 'dotenv';
+// basically being used to configure environmental variables and not store sensitive info like passwords in code. 
+// dotenv.config()
+
+
+
+const app = express();
+
+app.use(bodyParser.json({ extended: true }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(cors());
+
+// app.use('/users', userRoutes);
+
+// const CONN_URL = ``;
+
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONN_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server up and running on Port ${PORT}`)))
+    .catch((error) => console.log(error))
+
+
